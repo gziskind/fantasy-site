ae033b1266cf10206fc681700587e2dd80834d06
Greg Ziskind <gregory.ziskind@synergistcomputing.com>
2014-03-03 18:04:41 -0500

    Changed structure of website

--- fantasy_server.rb
+++ fantasy_server.rb
@@ -6,7 +6,42 @@ class FantasyServer < Sinatra::Base
 		run!
 	end
 
-	get '/' do
-		erb :index
+	get '/:sport/results' do
+		@sport = params[:sport];
+		@seasons = [2013,2012,2011,2010];
+
+		erb :results
+	end
+
+	get '/:sport/records' do
+		@sport = params[:sport];
+		@users = ['Greg','Greg2','Greg3'];
+
+		erb :records
+	end
+
+	get '/:sport/names' do 
+		@sport = params[:sport];
+		@users = ['Greg','Greg2','Greg3','Greg4'];
+
+		erb :names
+	end
+
+	helpers do
+		def isBaseballActive
+			if @sport == 'baseball'
+				return 'active'
+			else
+				return ''
+			end
+		end
+
+		def isFootballActive 
+			if @sport == 'football'
+				return 'active'
+			else
+				return ''
+			end
+		end
 	end
 end
