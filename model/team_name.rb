c8552649f2abecf264f2a5155dc76ec76da26aca
Greg Ziskind <gregory.ziskind@synergistcomputing.com>
2014-06-17 21:15:51 -0400

    Updated model to include rating on team names instead of likes and dislikes

--- model/team_name.rb
+++ model/team_name.rb
@@ -5,13 +5,10 @@ class TeamName
 
 	key :name, String, :required => true
 	key :sport, String, :required => true
-	key :likes_ids, Array
-	key :dislikes_ids, Array
 
 	belongs_to :owner, :class_name => 'User'
 	belongs_to :hall_of_names
-	many :likes, :class_name => 'User', :in => :likes_ids
-	many :dislikes, :class_name => 'User', :in => :dislikes_ids
+	many :ratings
 
 	timestamps!
 end
