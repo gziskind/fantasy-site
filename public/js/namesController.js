f734d8077aacd2c0c8af4300c693134509f7336d
Greg Ziskind <gregory.ziskind@synergistcomputing.com>
2014-06-18 22:00:28 -0400

    Added 5 star rating system

--- public/js/namesController.js
+++ public/js/namesController.js
@@ -1,28 +1,50 @@
 angular.module('aepi-fantasy').controller('NamesController', function($scope, $location, $routeParams, $resource) {
 
+	// Private Variables
+	var sport = $scope.$parent.getSportType();
+
 	// Public variables
-	$scope.sport = capitaliseFirstLetter($scope.$parent.getSportType());
+	$scope.sport = capitaliseFirstLetter(sport);
 	$scope.names = [];
 	$scope.user = $routeParams.user;
 
-	// Public functions
-
 	// Watches
 	$scope.$watch('user', updateNames)
+	$scope.$watch('currentUser', reloadNames)
+	
+	// Public functions
+	$scope.changeRating = function(name) {
+		if(name.previousRating != name.myRating) {
+			name.previousRating = name.myRating
+			
+			var Rating = $resource('/api/' + sport + '/names/rating');
+			Rating.save(name, function(response) {
+				name.rating = response.totalRating
+			});
+		}
+	}
+
 
 	// Private Functions
 	function updateNames(newValue, oldValue) {
 		var url = '';
-		var sport = $scope.$parent.getSportType();
 		if(newValue) {
 			url = '/api/' + sport + '/names/' + newValue;
 		} else {
 			url = '/api/' + sport + '/names';
 		}
+		
 		var TeamNames = $resource(url);
-		var value = TeamNames.query();
+		var value = TeamNames.query(function(response) {
+			for(var c = 0; c < value.length; c++) {
+				value[c].previousRating = value[c].myRating
+			}
+			$scope.names = value;
+		});
+	}
 
-		$scope.names = value;
+	function reloadNames(newValue, oldValue) {
+		updateNames($scope.user);
 	}
 
 	function capitaliseFirstLetter(str) {
