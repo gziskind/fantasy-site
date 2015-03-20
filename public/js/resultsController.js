4e262719f10be31e4925c7a9683a58e3978e05c7
Greg Ziskind <gbziskind@gmail.com>
2015-03-19 21:48:47 -0400

    Added viewing standings by regular season standings

--- public/js/resultsController.js
+++ public/js/resultsController.js
@@ -5,6 +5,7 @@ angular.module('aepi-fantasy').controller('ResultsController', function($scope, 
 	$scope.results = []
 	$scope.leagueName = '';
 	$scope.currentYear = new Date().getFullYear();
+	$scope.selectedStandingsType = 'final';
 
 	// Private Variables
 	var firstPlaceNumber = 0;
@@ -22,6 +23,7 @@ angular.module('aepi-fantasy').controller('ResultsController', function($scope, 
 
 	// Watches
 	$scope.$watch('year', updateResults);
+	$scope.$watch('selectedStandingsType', updateStandingsOrder);
 
 	// Private Functions
 	function updateResults(newValue, oldValue) {
@@ -33,7 +35,63 @@ angular.module('aepi-fantasy').controller('ResultsController', function($scope, 
 			}
 
 			$scope.leagueName = value.leagueName;
-			$scope.results = value.results
+			$scope.results = value.results;
 		});
+	}
+
+	function updateStandingsOrder(newValue, oldValue) {
+		$scope.results = sortByStandingsType($scope.results);
+	}
+
+	function sortByStandingsType(results) {
+		if($scope.selectedStandingsType == 'final') {
+			return sortByPlace(results);
+		} else {
+			return sortByRegularSeason(results);
+		}
+
+		return finalResults;
+	}
+
+	function sortByPlace(results) {
+		results.sort(function(result1, result2) {
+			if(result1.place < result2.place) {
+				return -1;
+			} else {
+				return 1;
+			}
+		});
+
+		return results;
+	}
+
+	function sortByRegularSeason(results) {
+		for(var c = 0; c < results.length; c++) {
+			results[c].winPercentage = calculateWinPercentage(results[c]);
+		}
+
+		results.sort(function(result1, result2) {
+			if(result1.winPercentage < result2.winPercentage) {
+				return 1;
+			} else if(result1.winPercentage > result2.winPercentage) {
+				return -1;
+			} else {
+				if(result1.points && result2.points) {
+					if(result1.points < result2.points) {
+						return 1;
+					} else {
+						return -1;
+					}
+				} else {
+					return 0;
+				}
+			}
+		});
+
+		return results;
+	}
+
+	function calculateWinPercentage(result) {
+		return (result.wins + (result.ties/2.0))/(result.wins + result.losses + result.ties + 1.0);
 	}
 });
