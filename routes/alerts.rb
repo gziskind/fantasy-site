b6ea330f6960d139e5a77e06f5ff3939e0602bab
Greg Ziskind <gbziskind@gmail.com>
2015-03-26 21:56:20 -0400

    Added alert to change password the first time

--- routes/alerts.rb
+++ routes/alerts.rb
@@ -8,6 +8,9 @@ class FantasyServer
 					baseball: get_records_count('baseball'),
 					football: get_records_count('football')
 				}
+			},
+			user: {
+				changePassword: need_to_change_password
 			}
 		}.to_json
 	end
@@ -16,5 +19,10 @@ class FantasyServer
 		FantasyRecord.find_all_by_sport_and_confirmed(sport, false).size
 	end
 
+	def need_to_change_password
+		if !@user.nil? && @user.password == Digest::MD5.hexdigest('testing')
+			1
+		end
+	end
 
 end
