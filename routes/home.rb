8c1171663875bf871c0a40ee8bd78b61bb8f7533
Greg Ziskind <gbziskind@gmail.com>
2015-04-18 21:01:50 -0400

    Added ability to view new records and new team names

--- routes/home.rb
+++ routes/home.rb
@@ -8,6 +8,7 @@ class FantasyServer
 		championship_results  = Result.find_all_by_place(1)
 		last_place_results = Result.find_all_by_place(12)
 		team_names = TeamName.all
+		records = FantasyRecord.find_all_by_confirmed(true)
 
 		championships = {}
 		championship_results.each {|result|
@@ -47,12 +48,38 @@ class FantasyServer
 				@team_names.push({
 					name: team_name.name,
 					owner: team_name.owner.name,
-					rating: get_total_rating(team_name).round(2)
+					rating: rating.round(2)
 				})
 			end
 		}
 
 		@team_names.sort_by! {|team_name| team_name[:rating]}
+
+		@new_team_names = []
+		new_team_names = team_names.sort_by {|team_name| team_name.created_at}
+		new_team_names.each {|team_name| 
+			rating = get_total_rating(team_name)
+
+			@new_team_names.push({
+				name: team_name.name,
+				owner: team_name.owner.name,
+				rating: rating.nil? ? '-' : rating.round(2)
+			})
+		}
+		@new_team_names.reverse!
+
+		@new_records = []
+		records.each {|record|
+			@new_records.push({
+				sport: record.sport == 'baseball' ? 'BB' : 'FB',
+				record: record.record,
+				value: record.value.to_i.round(2), 
+				owner: record.record_holders.length == 1 ? record.record_holders[0].user.name : 'Various',
+				created_at: record.created_at
+			})
+		}
+		@new_records.sort_by! {|record| record[:created_at].nil? ? Date.new(0) : record[:created_at]  }
+		@new_records.reverse!
 
 		erb :index
 	end
